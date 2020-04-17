937246d1bd84e7b19820d8b4d41ef24559e5fb9b
VidhyaRiya <vidhyariya@Haries-Mac-mini.local>
2020-04-17 11:06:04 +0300

    create appointment added

--- client/src/app/appointment-list/appointment-list.component.ts
+++ client/src/app/appointment-list/appointment-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../common/appointment';
+import { AppointmentsService } from '../common/appointments.service';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-appointment-list',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment-list.component.scss']
 })
 export class AppointmentListComponent implements OnInit {
+  public loading: boolean = true;
+  public errorMsg: string;
+  public successMsg: string;
+  public appointments: Appointment[];
+  public columns = ['appointmentDate', 'name', 'email', 'cancel'];
 
-  constructor() { }
+  constructor(private appointmentService: AppointmentsService) { }
 
   ngOnInit(): void {
+    this.getAppointments();
+  }
+
+  getAppointments(){
+    this.appointmentService.getAllAppointments().subscribe((res: Appointment[]) => {
+      this.appointments = res;
+      this.loading = false;
+      console.log(this.appointments);
+    }, (err: ErrorEvent) => {
+      this.errorMsg = err.error.message;
+      this.loading = false;
+    });
+  };
+
+  cancelAppointment(id: string){
+    this.appointmentService.deleteAppointment(id).pipe(
+      mergeMap(() => this.appointmentService.getAllAppointments())
+    ).subscribe((res: Appointment[]) => {
+      this.appointments = res;
+      this.successMsg = 'Successfully deleted appointment'
+    }, (err: ErrorEvent) => {
+      this.errorMsg = err.error.message;
+    })
   }
 
 }
--- client/src/app/appointments/appointments.component.ts
+++ client/src/app/appointments/appointments.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AppointmentsService } from '../common/appointments.service';
+import { Appointment } from '../common/appointment';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-appointments',
@@ -7,9 +11,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentsComponent implements OnInit {
 
-  constructor() { }
+  public successMsg: string;
+  public errorMsg: string;
+  newAppointment: Appointment = {
+    _id : '',
+    appointmentDate: '',
+    name: '',
+    email: ''
+  }
 
+
+  constructor(private appointmentService: AppointmentsService) { }
   ngOnInit(): void {
+
+  }
+
+  resetForm(form?: NgForm){
+    if (form = null)
+      form.resetForm();
+    this.newAppointment.appointmentDate = '';
+    this.newAppointment.name = '';
+    this.newAppointment.email = '';
+
+  }
+
+
+  createAppointment(){
+    delete this.newAppointment._id;
+    this.successMsg = '';
+    this.errorMsg = '';
+    console.log(this.newAppointment);
+
+    this.appointmentService.createAppointment(this.newAppointment).subscribe((res: Appointment) => {
+      this.resetForm();
+      const appointmentDate = new Date(res.appointmentDate).toDateString();
+      this.successMsg = `Appointment created successfully for ${appointmentDate}`
+    }, (err: ErrorEvent) => {
+      this.errorMsg = err.error.message;
+    })
   }
 
 }
